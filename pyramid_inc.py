f16d4f54ceb5115b027af42ddc373c6ebd2f4211
Daniel Campos <daniel.campos12@fatec.sp.gov.br>
2019-04-07 17:46:03 -0300

    Update pyramid_inc.py

--- pyramid_inc.py
+++ pyramid_inc.py
@@ -1,6 +1,25 @@
 from Cores import cores
 from time import sleep
-rows = int(input('How many rows are in it? ')) # The size of the pyramid
+
+def readInt(msg):
+    ok = False
+    value = 0
+    while True:
+        rows = str(input(msg))
+        if rows.isnumeric():
+            value = int(rows)
+            ok = True
+        else:
+            print('{}ERROR! Type an integer valid number.{}'.format(cores['amarelo'], cores['limpa']))
+        if ok:
+            break
+    return value
+
+rows = readInt('How many rows are in it? ') # The size of the pyramid
+
+if rows > 100: # Time to conclude the task
+    print('{}Please, sit! It will take more time than Windows Updating!{}'.format(cores['azul'], cores['limpa']))
+
 
 print('{}PROCESSING...\n{}'.format(cores['pretoebranco'], cores['limpa']))
 sleep(2) # A function from time library to delay it for 2 seconds
@@ -14,5 +33,5 @@ for i in range(1, rows + 1): # To set the range
         print("{}*{}".format(cores['pretoebranco'], cores['limpa']), end = "")
     for m in range(rows-i): # Last underscore
         print("{}_{}".format(cores['amarelo'], cores['limpa']), end = "")
-    sleep(1) # A function from time library to delay it for 2 seconds
+    sleep(1) # A function from time library to delay each row for 1 second
     print()
